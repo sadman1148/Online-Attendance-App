aa7606829e1a720d5807ba4b98878b7fa3b40b96
Sadman Alam Reshad <sadman.alam@bjitgroup.com>
2023-09-18 14:08:55 +0600

    Base project upload

---
+++ OnlineAttendanceApp/app/src/main/java/com/bondstein/onlineattendanceapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bondstein.onlineattendanceapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bondstein.onlineattendanceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
